e867574de46ce0a13a6d4e42e9e40ce139d02f7c
DTH235647-TRINHMINHPHUONGHAO <hao2220sg@gmail.com>
2026-09-09 23:20:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,14 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    title: 'Có phải anh đã cứu thế giới Ở một cuộc đời trước đây😙',
+    desc: 'Nên là em mới bước tới  Dịu dàng và nắm tay :33',
+    btnYes: 'Thích tui thì nói <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em để ý tui <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Anh biết mà 🥰. Tui cũm thích em nhiều lắm 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
